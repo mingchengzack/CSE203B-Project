b9bdc28604a99d9a04c5a0d3d53a11472367755b
Albert Pi <944302874@qq.com>
2020-03-15 14:57:31 -0700

    naive SGD

---
+++ main.py
@@ -0,0 +1,97 @@
+import numpy as np
+import string
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
+from sklearn import metrics
+from sklearn import linear_model
+import torch
+from torch import nn
+import torch.optim as optim
+
+def MSE_loss(preds,labels):
+    return ((preds-labels)**2).mean()
+
+def Predict_and_Eval(V_T,labels):
+    # V_T shape: num_cluster * num_document, labels shape: 1* num_document
+    pass
+
+
+#Define latent facor model in PyTorch style
+class NMF_SGD(nn.Module):
+    def __init__(self,num_document,num_feature,num_cluster): #K is the latent vector dimension, alpha_init is used to initialize alpha
+        super(NMF_SGD,self).__init__()
+        self.U=nn.Parameter(torch.rand(num_feature,num_cluster))
+        self.V_T=nn.Parameter(torch.rand(num_cluster,num_document))
+    
+    def forward(self):
+        return torch.mm(self.U,self.V_T)
+
+#Training functions to conduct gradient descent in Latent Factor Model and DenseNET
+def NMF_training(matrix,labels,method):
+    num_document=matrix.shape[1]
+    num_feature=matrix.shape[0]
+    num_cluster=20
+
+    matrix=torch.tensor(matrix,dtype=torch.float32)
+
+    #Specify the model, learning rate and optimizer
+    model=NMF_SGD(num_document,num_feature,num_cluster)
+    learning_rate=5e-1
+    optimizer=optim.SGD(model.parameters(),lr=learning_rate,momentum=0.8,weight_decay=5e-4)
+    
+    #Define loss function
+    loss_func=MSE_loss
+
+    train_epochs=2000
+    for epoch in range(train_epochs):
+
+        optimizer.zero_grad() #Zero gradient to avoid accumulating
+        preds=model() #Forward
+        loss_train=loss_func(preds,matrix) #Compute loss on training set
+        loss_train.backward() #Back propagation
+        optimizer.step() #Update parameters
+
+        # Get V_T and convert to numpy array
+        for name, param in model.named_parameters():
+            if name=='V_T':
+                V_T=param.data.detach().numpy()
+
+        #Predict label and compute accuracy
+        acc=Predict_and_Eval(V_T,labels)
+
+        if epoch<5 or epoch%5==0:
+            print("Epoch: %d, train loss is: %f" %(epoch,float(loss_train)))
+            # print("Epoch: %d, train loss is: %f, evaluation accuracy is: %f" %(epoch,float(loss_train),float(acc)))
+
+def NMF(matrix,labels,method):
+    
+    #Perform NMF with different methods
+    if method=='SGD':
+        MSE=NMF_training(matrix,labels,'SGD')
+    
+    # print("MSE of "+method+" on validation set is: %f" %MSE)
+
+
+
+if __name__ == "__main__":
+
+
+    #Read dataset
+    # matrix shape: num_document * num_feature, labels: num_document * 1 
+    with np.load('./data/test.npz',allow_pickle=True) as data:
+        matrix=data['texts']
+        labels=data['labels']
+    print('Read dataset complete')
+    print('Matrix size: '+str(matrix.shape))
+
+# #-------------------------plot statistics of dataset-----------------------------
+#     plot_dataset_statistics(dataset)
+
+#-------------------------NMF SGD-----------------------------
+    NMF(matrix,labels,'SGD')
+
--- preprocess.py
+++ preprocess.py
@@ -5,12 +5,16 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 def main():
     newsgroups_train = fetch_20newsgroups(data_home="./data/", subset='train', remove=('headers', 'footers', 'quotes'))
     newsgroups_test = fetch_20newsgroups(data_home="./data/", subset='test', remove=('headers', 'footers', 'quotes'))
-    vectorizer = TfidfVectorizer()
-    print(newsgroups_test.data[0])
-    x_train = vectorizer.fit_transform(newsgroups_train.data)
+    vectorizer = TfidfVectorizer(max_features=5000)
+    x_train = vectorizer.fit_transform(newsgroups_train.data).toarray()
     y_train = newsgroups_train.target
-    x_test = vectorizer.fit_transform(newsgroups_test.data)
+    x_test = vectorizer.fit_transform(newsgroups_test.data).toarray()
     y_test = newsgroups_test.target
+
+    x_train=np.transpose(x_train)
+    y_train=np.transpose(y_train)
+    x_test=np.transpose(x_test)
+    y_test=np.transpose(y_test)
 
     np.savez_compressed('data/train.npz', texts=x_train, labels=y_train)
     np.savez_compressed('data/test.npz', texts=x_test, labels=y_test)
