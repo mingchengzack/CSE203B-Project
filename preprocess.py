c7284342294637c5d0dd3d0f386c07d29d47eedd
Albert Pi <944302874@qq.com>
2020-03-14 22:18:04 -0700

    paper

--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,7 @@ def main():
     newsgroups_train = fetch_20newsgroups(data_home="./data/", subset='train', remove=('headers', 'footers', 'quotes'))
     newsgroups_test = fetch_20newsgroups(data_home="./data/", subset='test', remove=('headers', 'footers', 'quotes'))
     vectorizer = TfidfVectorizer()
+    print(newsgroups_test.data[0])
     x_train = vectorizer.fit_transform(newsgroups_train.data)
     y_train = newsgroups_train.target
     x_test = vectorizer.fit_transform(newsgroups_test.data)
